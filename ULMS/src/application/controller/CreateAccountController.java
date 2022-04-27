20d8dd84bb9f14344f5381f7d3b1637aef08facc
ReneRod22 <100320874+ReneRod22@users.noreply.github.com>
2022-04-26 20:43:38 -0500

    Update CreateAccountController.java

--- ULMS/src/application/controller/CreateAccountController.java
+++ ULMS/src/application/controller/CreateAccountController.java
@@ -1,51 +1,68 @@
-package application.controller;
+package application;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class CreateAccountController {
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
-    @FXML
-    private Button submitButton;
+public class RegisterController {
+	
+	FileChooser fc=new FileChooser();
+	private AnchorPane mainPane;
 
-    @FXML
-    private TextField userNameField;
-
-    @FXML
-    private TextField passwordField;
-
-    @FXML
-    private TextField emailField;
-
-    @FXML
-    private Button homePageButton;
-    
-    @FXML
-    private AnchorPane createAccountPane;
-
-    @FXML
-    void submit(ActionEvent event) {
-
-    }
-
-    @FXML
-    void home(ActionEvent event) throws IOException {
-
-    	createAccountPane = FXMLLoader.load(getClass().getResource("/application/view/Main.fxml"));
-    	Scene scene = new Scene(createAccountPane,600,425);
-    	scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
-    	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	window.setScene(scene);
-    	window.show();
-    }
-
+	@FXML
+	private TextField TextField1;
+	
+	@FXML
+	private TextField TextField2;
+	
+	@FXML
+	private TextField TextField3;
+	
+	@FXML
+	private TextArea textarea;
+	
+	
+	@FXML
+	private void saveUserInfo(ActionEvent event) throws IOException {
+		String s1=TextField1.getText().toString()+"\t ";
+		String s2=TextField2.getText().toString()+"\t ";
+		String s3=TextField3.getText().toString()+"\t ";
+		FileWriter file=new FileWriter("users.csv",true);
+		file.write(s1+"\t");
+		file.write(s2+"\t");
+		file.write(s3);
+		file.write(System.getProperty("line.separator"));
+		file.close();
+		TextField1.clear();
+		TextField2.clear();
+		TextField3.clear();
+		Alert alert=new Alert(AlertType.CONFIRMATION);
+		alert.showAndWait();
+	}
+	
+	@FXML
+	private void sendToMain(ActionEvent event) throws IOException {
+		mainPane = FXMLLoader.load(getClass().getClassLoader().getResource("Main.fxml"));
+		Scene scene = new Scene(mainPane);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
 }
