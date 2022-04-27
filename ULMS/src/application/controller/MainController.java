b417e2a117842c9d24ef623f0add55407920355e
ReneRod22 <100320874+ReneRod22@users.noreply.github.com>
2022-04-26 20:41:34 -0500

    Update MainController.java

--- ULMS/src/application/controller/MainController.java
+++ ULMS/src/application/controller/MainController.java
@@ -1,54 +1,37 @@
-package application.controller;
-
-import java.io.IOException;
+package application;
 
 import javafx.event.ActionEvent;
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainController {
+	@FXML
+	private AnchorPane mainPane;
+	private AnchorPane mainPane2;
+	
+	@FXML
+	private void sendToLogin(ActionEvent event) throws IOException {
+		mainPane = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
+		Scene scene = new Scene(mainPane);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+	
+	@FXML
+	private void sendToRegister(ActionEvent event) throws IOException {
+		mainPane2 = FXMLLoader.load(getClass().getClassLoader().getResource("Register.fxml"));
+		Scene scene = new Scene(mainPane2);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
 
-    @FXML
-    private Button createAccountButton;
-
-    @FXML
-    private Button loginButton;
-    
-    @FXML
-    private TextField userNameField;
-
-    @FXML
-    private TextField passwordField;
-    
-    @FXML
-    private AnchorPane welcomePane;
-
-    @FXML
-    void login(ActionEvent event) throws IOException {
-
-    	welcomePane = FXMLLoader.load(getClass().getResource("/application/view/SystemInterface.fxml"));
-    	Scene scene = new Scene(welcomePane,600,425);
-    	scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
-    	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	window.setScene(scene);
-    	window.show();
-    }
-
-    @FXML
-    void createAccount(ActionEvent event) throws IOException {
-    	
-    	welcomePane = FXMLLoader.load(getClass().getResource("/application/view/CreateAccount.fxml"));
-    	Scene scene = new Scene(welcomePane,600,425);
-    	scene.getStylesheets().add(getClass().getResource("/application/application.css").toExternalForm());
-    	Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	window.setScene(scene);
-    	window.show();
-
-    }
 }
